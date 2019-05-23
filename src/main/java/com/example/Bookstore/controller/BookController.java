0bb577ad092676f42e1f6fb4c673082d164e528a
MP <MP1@MacBook-Pro-3.lan>
2019-05-23 21:18:53 +0300

    Week 4 update

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -1,15 +1,20 @@
 package com.example.Bookstore.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.CategoryRepository;
+
 
 
 @Controller
@@ -25,6 +30,18 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	// RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    } 
+	
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }       
 
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
@@ -42,6 +59,7 @@ public class BookController {
 	@RequestMapping(value = "/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("categories", crepository.findAll());
 		return "editbook";
 	}
 
--- src/main/java/com/example/Bookstore/domain/Category.java
+++ src/main/java/com/example/Bookstore/domain/Category.java
@@ -51,5 +51,11 @@ public class Category {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+		return "Category [categoryid=" + id + ", name=" + name + "]";
+	}
+	
 
 }
